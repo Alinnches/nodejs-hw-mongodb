41d57efc0ca1b6ae3e814d9ae716e215f30672bd
Alina Strijkiv <alinastrijkiv@gmail.com>
2025-07-05 11:06:23 +0300

    fixed

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -7,17 +7,26 @@ export const createContactSchema = Joi.object({
     'string.max': 'Name must have at most {#limit} characters',
     'any.required': 'Name is requiered',
   }),
-  email: Joi.string().email().required().messages({
-    'string.email': 'Email must be a valid email address',
-    'any.required': 'Email is required',
-  }),
-  phone: Joi.string()
+  phoneNumber: Joi.string()
     .pattern(/^\+?[0-9\s\-()]{7,20}$/)
     .required()
     .messages({
       'string.pattern.base':
         'Phone must be a valid format (digits, spaces, +, -, ())',
       'any.required': 'Phone is required',
+    }),
+  email: Joi.string().email().optional().messages({
+    'string.email': 'Email must be a valid email address',
+    'any.required': 'Email is required',
+  }),
+  isFavourite: Joi.boolean().optional().messages({
+    'boolean.base': 'isFavourite must be true or false',
+  }),
+  contactType: Joi.string()
+    .valid('work', 'home', 'personal')
+    .optional()
+    .messages({
+      'any.only': 'Contact type must be one of: work, home, personal',
     }),
 });
 
@@ -26,13 +35,18 @@ export const updateContactSchema = Joi.object({
     'string.min': 'Name must have at least {#limit} characters',
     'string.max': 'Name must have at most {#limit} characters',
   }),
-  email: Joi.string().email().messages({
-    'string.email': 'Email must be a valid email address',
-  }),
   phone: Joi.string()
     .pattern(/^\+?[0-9\s\-()]{7,20}$/)
     .messages({
-      'string.pattern.base':
-        'Phone must be a valid format (digits, spaces, +, -, ())',
+      'string.pattern.base': 'Phone number must be in a valid format',
     }),
+  email: Joi.string().email().messages({
+    'string.email': 'Email must be a valid email address',
+  }),
+  isFavourite: Joi.boolean().messages({
+    'boolean.base': 'isFavourite must be true or false',
+  }),
+  contactType: Joi.string().valid('work', 'home', 'personal').messages({
+    'any.only': 'Contact type must be one of: work, home, personal',
+  }),
 });

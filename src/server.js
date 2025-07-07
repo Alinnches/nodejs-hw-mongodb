3c622b5f563942dfa84bd45ee4c250fb3108e064
Alina Strijkiv <alinastrijkiv@gmail.com>
2025-07-07 19:07:56 +0300

    fixed

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ export const setupServer = () => {
     }),
   );
 
-  app.use('/api/contacts', contactsRouter);
+  app.use('/contacts', contactsRouter);
   app.use(errorHandler);
   app.use(notFoundHandler);
 

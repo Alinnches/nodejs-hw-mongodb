1196eaa138fd75230beb36bf9134e600b8deef6a
Alina Strijkiv <alinastrijkiv@gmail.com>
2025-07-17 17:09:49 +0300

    finished

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -23,25 +23,33 @@ export const registerUserController = async (req, res) => {
   });
 };
 
-export const loginUserController = async (req, res) => {
-  const session = await loginUser(req.body);
+export const loginUserController = async (req, res, next) => {
+  try {
+    const session = await loginUser(req.body);
 
-  res.cookie('refreshToken', session.refreshToken, {
-    httpOnly: true,
-    expires: new Date(Date.now() + ONE_DAY),
-  });
-  res.cookie('sessionId', session._id, {
-    httpOnly: true,
-    expires: new Date(Date.now() + ONE_DAY),
-  });
+    if (!session) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
 
-  res.json({
-    status: 200,
-    message: 'Successfully logged in a user!',
-    data: {
-      accessToken: session.accessToken,
-    },
-  });
+    res.cookie('refreshToken', session.refreshToken, {
+      httpOnly: true,
+      expires: new Date(Date.now() + ONE_DAY),
+    });
+    res.cookie('sessionId', session._id, {
+      httpOnly: true,
+      expires: new Date(Date.now() + ONE_DAY),
+    });
+
+    res.json({
+      status: 200,
+      message: 'Successfully logged in a user!',
+      data: {
+        accessToken: session.accessToken,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const logoutUserController = async (req, res) => {
